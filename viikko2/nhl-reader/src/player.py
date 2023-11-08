48a372c10e36aed6a9a7b318e848dd72408b45c2
Pauli Karels <pauli.karels@gmail.com>
2023-11-09 01:59:54 +0200

    Ex. 10 Done

---
+++ viikko2/nhl-reader/src/player.py
@@ -0,0 +1,15 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+
+    
+    def __str__(self):
+        return (
+            f"{self.name}"
+            f" team {self.team or '-'}"
+            f" goals {self.goals}"
+            f" assists {self.assists}"
+        )
