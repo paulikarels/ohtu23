99d6df67890155e9922583b764c663561b5132ef
Pauli Karels <pauli.karels@gmail.com>
2023-11-26 01:58:28 +0200

    W4. Ex 3-5 Done

---
+++ viikko4/retro.md
@@ -0,0 +1,7 @@
+Retrospektiivitekniikat
+
+Käsitteellä tarkoitetaan menetelmiä/tekniikoita joiden avulla projektitiimit keskenään keskustelevat tietyn aikavälin jälkeen projektista ja varsinkin siitä kuinka he onnistuivat tai näkisivät mitä parantaa tai ottaa huomioon tulevissa ajallisissa iteraatioissa. 
+
+Start, Stop, Continue, More of, Less of Wheel - tekniikassa projektitiimi arvioi, mitä heidän pitäisi aloittaa, lopettaa ja jatkaa, sekä mitä he tarvitsevat enemmän tai vähemmän parantaakseen prosessejaan. Toteutustapa tekniikassa tehdään kirjaten; hyödyntäen tauluja tai fläppitaulupapereita, jotka ovat jaettu omiin lohkoihin. 
+
+Glad, Sad, Mad - tekniikalla yksilöiden havainnot listataan kolmeen eri lohkoon (Glad, Sad, Mad). Kyseisillä lohkoilla tavoitellaan käsitellä sprintin aikana tuomista negatiivisista, positiivisista ja muista huomiosta tuomia havaintoja.  Projektitiimi käsittelevät asiat ja huomiot äänestämällä “tärkeimmät” huomiot ja käsittelevät ne saatavan ajan mukaisesti (~ 60 minuuttia).
---
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -0,0 +1,98 @@
+import unittest
+from unittest.mock import Mock, ANY
+from kauppa import Kauppa
+from viitegeneraattori import Viitegeneraattori
+from varasto import Varasto
+from tuote import Tuote
+from ostoskori import Ostoskori
+
+class TestKauppa(unittest.TestCase):
+    def setUp(self):
+        self.pankki_mock = Mock()
+        self.viitegeneraattori_mock = Mock()
+        self.varasto_mock = Mock()
+
+        # palautetaan aina arvo 42
+        self.viitegeneraattori_mock.uusi.return_value = 42
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+            if tuote_id == 2:
+                return 25
+            if tuote_id == 3:
+                return 0
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+            if tuote_id == 2:
+                return Tuote(2, "juusto", 20)
+            if tuote_id == 3:
+                return Tuote(3, "kurkku", 3)
+        
+        # otetaan toteutukset käyttöön
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+        self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        # alustetaan kauppa
+        self.kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+
+    def test_ostoksen_paaytyttya_pankin_metodia_tilisiirto_kutsutaan(self):
+
+        # tehdään ostokset
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        # varmistetaan, että metodia tilisiirto on kutsuttu
+        self.pankki_mock.tilisiirto.assert_called()
+        # toistaiseksi ei välitetä kutsuun liittyvistä argumenteista
+
+    def test_lisataan_tuote_ostoksen_valityksella(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", ANY, "12345", ANY, 5)
+
+    def test_kahden_eri_tuotteen_tilisiiro(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", ANY, "12345", ANY, 25)
+
+
+    def test_kahden_saman_tuotteen_tilisiiro(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", ANY, "12345", ANY, 10)
+
+    def test_kahden_tuotteen_tilisiiro_joista_toinen_loppu(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(3)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", ANY, "12345", ANY, 5)
+
+    def test_kauppa_pyytää_uutta_viitenumeroa_kaikille_maksutapahtumille(self):
+        self.kauppa.aloita_asiointi()   
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+        
+        self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 1)
+
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("testitero", "54321")
+
+        self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 2)
